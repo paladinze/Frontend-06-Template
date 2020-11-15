6802826e7bab9047e1e693a33693a672e4e29811
paladinze <paladinze@hotmail.com>
2020-11-15 14:54:32 +0800

    refactor: change tokenizer into a generator func

--- Week 03/index.js
+++ Week 03/index.js
@@ -3,19 +3,38 @@
 const symbols = ['Number', 'Whitespace', 'LineTerminator', '*', '/', '+', '-'];
 const regexp = /([0-9.]+)|([ \t])|([\r\n])|(\*)|(\/)|(\+)|(-)/g;
 
-const tokenize = (source) => {
+function* tokenize (source) {
+    let prevlastIndex;
     while (true) {
+        let token = {
+            type: null,
+            value: null,
+        };
+        prevlastIndex = regexp.lastIndex;
         const result = regexp.exec(source);
         if (!result) break;
+        if (regexp.lastIndex - prevlastIndex > result[0].length ) {
+            // encounters unknown symbols
+            break;
+        }
+
         for (let i=0; i<symbols.length; i++) {
             if (result[i+1]) {
-                console.log(symbols[i]);
+                token.type = symbols[i];
+                token.value = result[i+1];
             }
         }
-        console.log(result);
+        yield token;
+    }
+    yield {
+        type: 'EOF',
+        value: null,
     }
 }
 
 const source = '10 + 2 * 3'
 
-tokenize(source)
+for (let token of tokenize(source)) {
+    console.log(token);
+}
+
